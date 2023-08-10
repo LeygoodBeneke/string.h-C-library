f9f27d94042032e70c27bb01b6f2c65c1d7eab7b
VasiliyDenisov <mr.v.m.denisov@mail.ru>
2023-08-10 16:20:59 +0300

    Add num processing

--- src/functions/s21_sprintf.c
+++ src/functions/s21_sprintf.c
@@ -2,9 +2,13 @@
 #include "../s21_string.h"
 
 #define SPECS_LEN 16
-#define FLAGS_LEN 16
+#define FLAGS_LEN 5
+#define DESCRIPTION_LEN 3
 const char specs[SPECS_LEN] = "cdieEfgGosuxXpn%";
 const char flags[FLAGS_LEN] = "-+ #0";
+const char desc[DESCRIPTION_LEN] = "hlL";
+
+#define DUMP(varname) (#varname)
 
 typedef struct {
     char spec;
@@ -14,40 +18,54 @@ typedef struct {
     char len_description;
     int is_star_width;
     int is_star_precision;
+    int string_len;
 } input_value;
+
+void write_data(char *str, input_value *dest);
+void _print_input_value(input_value tmp);
+char* num_proccessing(char *string, int *value);
+char *specs_check(char *string, char *value, int len, char* spec);
+char *star_check(char *string, int *value);
 
 int s21_sprintf(char *str, const char *format, ...) {
     va_list string_list;
     va_start(string_list, format);
     
     int idx = 0, read = 0;
-    for (char *ch = (char *)format; *ch; idx++) {
+    for (char *ch = (char *)format; *ch;) {
 
         if (*ch == '%') {
             read = 1;
         }
 
         if (!read) {
-            str[idx] = *ch;
+            printf("CHAR: %c\n", *ch);
+            str[idx++] = *ch;
             ch++;
         } else {
             char *buf = ch + 1;
             input_value data = {};
 
+            printf("BEFORE: %s\n", buf);
 
-            continue;
+            write_data(buf, &data);
+            _print_input_value(data);
+
+            ch += data.string_len;
+            printf("AFTER: %s\n", ch);
+            read = 0;
         }
 
 
 
     }
-    printf("%s %s", format, str);
+    //printf("%s %s", format, str);
     va_end(string_list);
     return 0;
 }
 
 void write_data(char *str, input_value *dest) {
-    int flags_flag = 1, idx = 0;
+    int flags_flag = 1;
     char *str_copy = str;
     while (flags_flag && str_copy) {
         flags_flag = 0;
@@ -57,35 +75,61 @@ void write_data(char *str, input_value *dest) {
                 flags_flag = 1;
             }
         }
+        if (flags_flag) str_copy++;
+    }
+    str_copy = num_proccessing(str_copy, &dest->width);
+    str_copy = star_check(str_copy, &dest->is_star_width);
+    if (str_copy && *str_copy == '.') {
         str_copy++;
+        str_copy = num_proccessing(str_copy, &dest->precision);
     }
-    int width_flag = 1;
-    while (str_copy && width_flag) {
-        if (*str_copy < '0' || *str_copy > '9')
-            width_flag = 0;
-        else {
-            dest->width *= 10;
-            dest->width += (*str_copy - '0');
-            str_copy++;
-        }
-    }
-    int len_decs_flag = 0;
-    if (str_copy) len_decs_flag = *str_copy == '.';
-    if (len_decs_flag) str_copy++;
-    while (str_copy && len_decs_flag) {
-        if (*str_copy < '0' || *str_copy > '9')
-            len_decs_flag = 0;
-        else {
-            dest->len_description *= 10;
-            dest->len_description += (*str_copy - '0');
-            str_copy++;
-        }
-    }
-    for (int i = 0; i < SPECS_LEN; i++) {
-        if (*str_copy == specs[i]) {
-            dest->spec = specs[i];
-        }
-    }
+    str_copy = star_check(str_copy, &dest->is_star_precision);
+    str_copy = specs_check(str_copy, &dest->len_description, DESCRIPTION_LEN, (char *)desc);
+    str_copy = specs_check(str_copy, &dest->spec, SPECS_LEN, (char *)specs);
+    dest->string_len = str_copy - str + 2;
+    str = str_copy;
+}
 
-// * не учтены
+char* num_proccessing(char *string, int *value) {
+    int flag = 1;
+    while (string && flag) {
+        if (*string < '0' || *string > '9')
+            flag = 0;
+        else {
+            *value *= 10;
+            *value += (*string - '0');
+            string++;
+        }
+    }
+    return string;
+}
+
+char *specs_check(char *string, char *value, int len, char* spec) {
+    for (int i = 0; string && i < len; i++) {
+        if (*string == spec[i]) {
+            *value = spec[i];
+            string++;
+            break;
+        }
+    }
+    return string;
+}
+
+char *star_check(char *string, int *value) {
+    if (string && *string == '*') {
+        *value = 1;
+        string++;
+    }
+    return string;
+}
+
+
+void _print_input_value(input_value tmp) {
+    fprintf(stderr, "%s = %c\n", DUMP(tmp.spec), tmp.spec);
+    fprintf(stderr, "%s = %d\n", DUMP(tmp.width), tmp.width);
+    fprintf(stderr, "%s = %d\n", DUMP(tmp.precision), tmp.precision);
+    fprintf(stderr, "%s = %c\n", DUMP(tmp.len_description), tmp.len_description);
+    fprintf(stderr, "%s = %d\n", DUMP(tmp.is_star_width), tmp.is_star_width);
+    fprintf(stderr, "%s = %d\n", DUMP(tmp.is_star_precision), tmp.is_star_precision);
+    fprintf(stderr, "%s = %d\n", DUMP(tmp.string_len), tmp.string_len);
 }
